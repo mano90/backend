a2e8b1227a16ea54a1aa487b95241aa7b9fe4aba
mano90 <antsomanoirina90gmail.com>
2023-01-13 15:23:25 +0300

    Routes configuration

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+import { Request, Response } from "express";
 import { AppConfig } from "./Contrainte/Config/AppConfig/AppConfig";
-
+import { routes } from "./Contrainte/Config/Routes/AppRoute";
 const app = express();
 app.use(AppConfig.allowCrossDomain);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+routes.forEach((route) => {
+  (app as any)[route["method"]](
+    route["route"],
+    (req: Request, res: Response, next: Function) => {
+      const result = new (route["controller"] as any)()
+        [route["action"]](req, res, next)
+        .then(() => next)
+        .catch((err: any) => next(err));
+    }
+  );
+});
 
 app.listen(3000);
