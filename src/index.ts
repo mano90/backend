86b45bf16089fffd74abe7533940c974a3a6a28e
mano90 <antsomanoirina90gmail.com>
2023-01-13 15:16:53 +0300

    CORS configuration

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 const express = require("express");
-
 const bodyParser = require("body-parser");
+import { AppConfig } from "./Contrainte/Config/AppConfig/AppConfig";
 
 const app = express();
+app.use(AppConfig.allowCrossDomain);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
